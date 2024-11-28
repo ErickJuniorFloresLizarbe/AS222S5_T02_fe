8cc96638be84d081c50db16f71e164074b5c0efe
Erick Flores <126894236+ErickJuniorFloresLizarbe@users.noreply.github.com>
2024-11-28 01:09:25 +0000

    version 4.4

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    //production: false,
+    production: false,
     apiUrl: '${API_BACKEND}'
     //apiUrl: 'https://silver-space-acorn-x6j49xwj5gvh5x-8080.app.github.dev'
 };
